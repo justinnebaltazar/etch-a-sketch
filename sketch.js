385c39b4ce3c99a2d2329d2888fc6256b38ef434
justinnebaltazar <jfb3@sfu.ca>
2024-06-18 13:54:02 -0700

    Addevent listener for hovering over the square to change its background color

--- sketch.js
+++ sketch.js
@@ -2,16 +2,31 @@ let grid = document.getElementById("grid");
 
 function createGrid(length, width) {
     for (let i = 0; i < length; i++) {
+
         let row = document.createElement('div');
-        row.setAttribute('class', 'rows')
+        row.setAttribute('class', 'rows');
         grid.appendChild(row);
+
         for (let j = 0; j < width; j++) {
+
             let gridSquare = document.createElement('div');
             gridSquare.setAttribute('class', 'squares');
+            // DElETE
             gridSquare.textContent = i + 1;
             row.appendChild(gridSquare);
+
         }
     }
 }
 
+function changeColor() {
+    let squares = document.querySelectorAll(".squares"); 
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', () => {
+            square.setAttribute('class', 'hovered'); 
+        });
+    });
+}
+
 createGrid(16, 16);
+changeColor();
